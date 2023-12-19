8458045454f5d1549bdc50b3907184cb57df7a57
Calle Svensson <calle.svensson@zeta-two.com>
2023-12-20 00:27:51 +0100

    Terraform cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,6 @@ resource "google_dns_managed_zone" "play" {
   dns_name = "play.potluckctf.com."
 }
 
-# VPC
 resource "google_compute_network" "potluckctf_network" {
   name                    = "potluckctf-network"
   provider                = google-beta
@@ -41,12 +40,6 @@ resource "google_compute_subnetwork" "potluckctf_subnet" {
   network       = google_compute_network.potluckctf_network.id
 }
 
-#
-#resource "hcloud_floating_ip_assignment" "web-ip-assignment" {
-#  floating_ip_id = "[REDACTED]" # TODO (P0): Add new static ip
-#  server_id      = hcloud_server.livectf-web["web1.livectf.local"].id
-#}
-
 # generate inventory file for Ansible
 resource "local_file" "hosts_ansible_inventory" {
   content = templatefile("${path.module}/hosts.tpl",
@@ -62,23 +55,3 @@ resource "local_file" "hosts_ansible_inventory" {
   filename        = "hosts.yml"
   file_permission = "0644"
 }
-
-## generate SSH config file
-#resource "local_file" "hosts_ssh_config" {
-#  content = templatefile("${path.module}/hosts.ssh.tpl",
-#    {
-#      livectf-web             = hcloud_server.livectf-web
-#      livectf-builder         = hcloud_server.livectf-builder
-#      livectf-builder-volumes = hcloud_volume.livectf-builder
-#      livectf-runner          = hcloud_server.livectf-runner
-#      livectf-runner-volumes  = hcloud_volume.livectf-runner
-#      livectf-monitor         = hcloud_server.livectf-monitor
-#      livectf-all             = merge(hcloud_server.livectf-web, hcloud_server.livectf-builder, hcloud_server.livectf-runner, hcloud_server.livectf-monitor)
-#
-#      server_settings = merge(local.server_settings.web, local.server_settings.builders, local.server_settings.runners, local.server_settings.monitor)
-#      subnet          = hcloud_network_subnet.network-subnet
-#    }
-#  )
-#  filename        = "hosts.ssh.conf"
-#  file_permission = "0644"
-#}
