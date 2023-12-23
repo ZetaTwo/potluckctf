8f3a89b4b9e8899915524a81027bcf80badda925
Calle Svensson <calle.svensson@zeta-two.com>
2023-12-23 20:14:17 +0100

    Add script to check deliveries

---
+++ scripts/check-undelivered.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+gsutil ls gs://potluckctf-challenge-2
+gsutil ls gs://potluckctf-challenge-4
+gsutil ls gs://potluckctf-challenge-9
+gsutil ls gs://potluckctf-challenge-12
+gsutil ls gs://potluckctf-challenge-13
+gsutil ls gs://potluckctf-challenge-14
+gsutil ls gs://potluckctf-challenge-15
+gsutil ls gs://potluckctf-challenge-17
+gsutil ls gs://potluckctf-challenge-19
+gsutil ls gs://potluckctf-challenge-20
+gsutil ls gs://potluckctf-challenge-21
+gsutil ls gs://potluckctf-challenge-22
+gsutil ls gs://potluckctf-challenge-25
+gsutil ls gs://potluckctf-challenge-26
+gsutil ls gs://potluckctf-challenge-28
